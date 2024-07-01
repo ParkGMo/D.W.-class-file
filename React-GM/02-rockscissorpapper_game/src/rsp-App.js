9e98f63593f9cb9e2d763e3dfe44def6cee75500
BGM03 <iwn0180@naver.com>
2024-07-01 17:36:25 +0900

    7.1.17:36

--- React-GM/02-rockscissorpapper_game/src/rsp-App.js
+++ React-GM/02-rockscissorpapper_game/src/rsp-App.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import "./rsp-App.css";
+import rock from "./assets/rock.svg";
+import scissor from "./assets/scissor.svg";
+import paper from "./assets/paper.svg";
+import RspVersusIcon from "./RspVersusIcon.js";
 
 function App() {
   return (
     <div className="rsp-App">
-      <h1>Rock-Paper-Scissors Game</h1>
-      <p>Welcome to the Rock-Paper-Scissors Game!</p>
-      <p>Please select your move:</p>
-      <button onClick={() => alert("You chose Rock!")}>Rock</button>
-      <button onClick={() => alert("You chose Paper!")}>Paper</button>
-      <button onClick={() => alert("You chose Scissors!")}>Scissors</button>
-      <p>Developed by: Your Name</p>
+      <h1>가위 바위 보!</h1>
+      <div className="socre-block">
+        <div className="score-rsp my-rsp">0</div>
+        <div className="dots">
+          <div>●</div>
+          <div>●</div>
+        </div>
+        <div className="score-rsp other-rsp">0</div>
+      </div>
+      <RspVersusIcon />
+      <div className="score-batting">
+        <p className="batting-word">
+          배점
+          <input type="number" min="1" max="500" placeholder="1 ~ 500" /> 점
+        </p>
+      </div>
+      <button className="rsp-btn"> 승부기록! </button>
+      <div className="rsp-icons">
+        <img className="rsp-icon rock" src={rock} />
+        <img className="rsp-icon scissor" src={scissor} />
+        <img className="rsp-icon papper" src={paper} />
+      </div>
     </div>
   );
 }
