e3011e11095afddf155d1732af586e7800f12159
BGM03 <iwn0180@naver.com>
2024-07-03 17:15:34 +0900

    7.3.17:15

--- React-GM/03-moviepedia/src/App.js
+++ React-GM/03-moviepedia/src/App.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import "./firebaseGM.js";
 import "./App.css";
 import ReviewForm from "./ReviewForm";
+import ReviewList from "./ReviewList";
 import logoImg from "./assets/logo.png";
-import ticketImg from "./assets/ticket.png";
+import mockItems from "./mock.json";
+import { getDatas } from "./firebaseGM";
+
+function AppSortButton({ children }) {
+  // chidren 은 텍스트나 태그등 모두를 가져올 수 있다.
+  return <button className="AppSortButton selected">{children}</button>;
+}
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const handleLoad = async () => {
+    const resultData = await getDatas("Movie");
+    // console.log(resultData);
+    setItems(resultData);
+  };
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
+  // []-> 빈배열이면 변경되었을 때만 랜더링한다.
+  // [a, b, c] -> a,b,c 중 하나만 변경되어도 랜더링된다.
   return (
     <div className="App">
       <nav className="App-nav">
@@ -19,8 +41,14 @@ function App() {
         <div className="App-ReviewForm">
           <ReviewForm />
         </div>
-        <div className="App-sorts"></div>
-        <div className="App-ReviewList"></div>
+        <div className="App-sorts">
+          <AppSortButton>최신순</AppSortButton>
+          <AppSortButton>베스트순</AppSortButton>
+        </div>
+        <div className="App-ReviewList">
+          <ReviewList items={items} />
+          <button className="App-load-more-button">더보기</button>
+        </div>
       </div>
       <footer className="App-footer">
         <div className="App-footer-container">| 개인정보 처리방침</div>
---
+++ React-GM/03-moviepedia/src/firebaseGM.js
@@ -0,0 +1,77 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  setDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsMcx-bHZHSbVdt0YTi0YhbQgXjcqx29g",
+  authDomain: "moviepedia-1571e.firebaseapp.com",
+  projectId: "moviepedia-1571e",
+  storageBucket: "moviepedia-1571e.appspot.com",
+  messagingSenderId: "166513038812",
+  appId: "1:166513038812:web:95505f62dfb752a7dfeb19",
+  measurementId: "G-8B6MXJ5T0D",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+
+async function getDatas(collectionName) {
+  const collect = await collection(db, collectionName);
+  const snapshot = await getDocs(collect);
+  const resultData = snapshot.docs.map((doc) => ({
+    docID: doc.id,
+    ...doc.data(),
+  }));
+
+  return resultData;
+}
+
+async function addDatas(collectionName, dataObj) {
+  //   문서 ID 수동
+
+  try {
+    // const saveDoc = doc(db, 컬렉션명, 문서ID)
+    const saveDoc = await doc(db, collectionName, `3`);
+    console.log(`doc() 결과 : ${saveDoc}`);
+    const saveResult = await setDoc(saveDoc, dataObj);
+    console.log(`setDoc() 결과 : ${saveResult}`);
+
+    // 문서 ID 자동
+    const collect = await collection(db, collectionName);
+    await addDoc(collect, dataObj);
+
+    return true;
+  } catch (error) {
+    return false;
+  } finally {
+  }
+}
+
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  // deleteDoc(삭제할 문서);
+  await deleteDoc(docRef);
+}
+
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  // doc(db, 컬렉션명, 문서ID);
+  const docRef = await doc(db, collectionName, docId);
+  // getDocs(문서레퍼런스);
+  // const docData = await getDocs(docRef);
+  // updateDoc(문서데이터, 수정할정보);
+  updateDoc(docRef, updateInfoObj);
+}
+
+export { db, getDatas, addDatas, deleteDatas, updateDatas };
