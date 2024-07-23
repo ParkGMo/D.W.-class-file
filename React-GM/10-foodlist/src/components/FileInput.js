0e277cb1e7e9400121e08694a74a693e536572b7
ParkGMo <167054605+ParkGMo@users.noreply.github.com>
2024-07-23 23:24:49 +0900

    7.23.23:24

--- React-GM/10-foodlist/src/components/FileInput.js
+++ React-GM/10-foodlist/src/components/FileInput.js
@@ -1,16 +1,50 @@
-import React from "react";
-import "./FileInput.css";
+import React, { useEffect, useState } from "react";
 import placeholderImg from "../assets/preview-placeholder.png";
 import resetImg from "../assets/ic-reset-white.png";
+import "./FileInput.css";
 
-function FileInput(props) {
+function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
+
+  const handleChange = (e) => {
+    const nextValue = e.target.files[0];
+    onChange(name, nextValue);
+  };
+
+  const handleClearClick = () => {
+    onChange(name, null);
+  };
+
+  useEffect(() => {
+    if (!value) return;
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview(null);
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]);
   return (
     <div className="FileInput">
-      <img className="FileInput-preview" src={placeholderImg} />
-      <input className="FileInput-hidden-overlay" type="file" />
-      <button className="FileInput-clear-button">
-        <img src={resetImg} />
-      </button>
+      <img
+        className={`FileInput-preview ${preview ? "selected" : ""}`}
+        src={preview || placeholderImg}
+      />
+      <input
+        className="FileInput-hidden-overlay"
+        type="file"
+        onChange={handleChange}
+      />
+      {value && (
+        <button
+          className="FileInput-clear-button"
+          onClick={handleClearClick}
+          type="button"
+        >
+          <img src={resetImg} />
+        </button>
+      )}
     </div>
   );
 }
--- React-GM/10-foodlist/src/components/FoodForm.js
+++ React-GM/10-foodlist/src/components/FoodForm.js
@@ -1,35 +1,74 @@
 import React, { useState } from "react";
 import FileInput from "./FileInput";
 import "./FoodForm.css";
+import { addDatas } from "../api/firebase";
+
+const INITIAL_VALUES = {
+  title: "",
+  content: "",
+  calorie: 0,
+  imgUrl: null,
+};
+
+function sanitize(type, value) {
+  switch (type) {
+    case "number":
+      return Number(value) || 0;
+
+    default:
+      return value;
+  }
+}
 
 function FoodForm(props) {
-  //   const [calorie, setCalorie] = useState(0);
-  const handChange = (e) => {
+  const [values, setValues] = useState(INITIAL_VALUES);
+
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    handleChange(name, sanitize(type, value));
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(e.target.value);
+    const resultData = await addDatas("food", values);
   };
   return (
-    <form className="FoodForm" onChange={handChange}>
-      <FileInput className="FoodForm-preview" />
+    <form className="FoodForm" onSubmit={handleSubmit}>
+      <FileInput
+        className="FoodForm-preview"
+        onChange={handleChange}
+        name="imgUrl"
+        value={values.imgUrl}
+      />
       <div className="FoodForm-rows">
-        <div className="FoodForm-tilte-calorie">
+        <div className="FoodForm-title-calorie">
           <input
             className="FoodForm-title"
             type="text"
+            onChange={handleInputChange}
             placeholder="이름을 입력해주세요."
-            // onChange={handChange}
+            name="title"
+            value={values.title}
           />
           <input
             className="FoodForm-calorie"
             type="number"
-            // onChange={handChange}
+            onChange={handleInputChange}
+            name="calorie"
+            value={values.calorie}
           />
-          <button className="FoodForm-submit-button">확인</button>
+          <button className="FoodForm-submit-button" type="submit">
+            확인
+          </button>
         </div>
         <textarea
           className="FoodForm-content"
-          placeholder="내용을 작성해 주세요."
-          //   onChange={handChange}
+          onChange={handleInputChange}
+          placeholder="내용을 작성해주세요."
+          name="content"
+          value={values.content}
         />
       </div>
     </form>
