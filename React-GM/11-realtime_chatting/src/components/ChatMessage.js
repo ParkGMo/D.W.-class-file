f773f256016791c6fe1b99f6389ca98032af1f54
BGM03 <iwn0180@naver.com>
2024-07-30 17:29:33 +0900

    7.30.17:29

--- React-GM/11-realtime_chatting/src/components/ChatMessage.js
+++ React-GM/11-realtime_chatting/src/components/ChatMessage.js
@@ -3,13 +3,15 @@ import tempImg from "../assets/ralo-profile.png";
 
 function ChatMessage({ message, auth }) {
   const user = auth?.currentUser.uid;
-  const { photoUrl, text, uid } = message;
+  const { photoUrl, text, uid, createdAt } = message;
   const messageClass = uid === user ? "sent" : "received";
+
   return (
     <>
       <div className={`message ${messageClass}`}>
         <img src={photoUrl} />
         <p>{text}</p>
+        <span className="message-timestamp"></span>
       </div>
     </>
   );
