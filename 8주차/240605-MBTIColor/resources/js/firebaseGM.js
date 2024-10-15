b3a3c89bf73672b56f31d12cb5643e1471da398b
ParkGMo <167054605+ParkGMo@users.noreply.github.com>
2024-10-15 17:32:49 +0900

    10.15.17:32

--- 8주차/240605-MBTIColor/resources/js/firebaseGM.js
+++ 8주차/240605-MBTIColor/resources/js/firebaseGM.js
@@ -36,14 +36,12 @@ async function addDatas(collectionName, dataObj, userName) {
 
   try {
     // const saveDoc = doc(db, 컬렉션명, 문서ID)
-    const saveDoc = await doc(db, collectionName, userName);
-    // console.log(`doc() 결과 : ${saveDoc}`);
-    const saveResult = await setDoc(saveDoc, dataObj);
-    // console.log(`setDoc() 결과 : ${saveResult}`);
+    // const saveDoc = await doc(db, collectionName, userName);
+    // const saveResult = await setDoc(saveDoc, dataObj);
 
     // 문서 ID 자동
-    // const collect = await collection(db, collectionName);
-    // await addDoc(collect, dataObj);
+    const collect = await collection(db, collectionName);
+    await addDoc(collect, dataObj);
 
     return true;
   } catch (error) {
